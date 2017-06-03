4a302c51d7c99542d57e00e63ffb8c43839e3020
alskra <krasnoyurchenko.a.s@gmail.com>
2017-06-03 19:40:03 +0300

    upd

--- markup/components/filters/filters.js
+++ markup/components/filters/filters.js
@@ -1,6 +1,14 @@
 $(function () {
     $('body').on('click', '.filters .aside__header', function () {
-        $('html, body').toggleClass('open_left');
+        if ($('html, body').hasClass('open_left')) {
+            setTimeout(function () {
+                $('html, body').removeClass('open_left');
+            }, 200);
+        }
+        else {
+            $('html, body').addClass('open_left');
+        }
+
         $('.wrapper').toggleClass('wrapper_open_left');
     });
 });
